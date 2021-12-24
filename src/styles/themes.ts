cdb829da5542926d27bda8eabf4f5fbb2c020006
Anna Kleszcz <anna.kleszcz123@gmail.com>
2021-12-24 13:09:30 +0100

    Adjust file structure and themes colors

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,12 +1,12 @@
 import { DefaultTheme } from "styled-components";
 
 export const autumnTheme: DefaultTheme = {
-  boxShadow: 'rgb(0 0 0 / 35%) 0px 0px 29px 0px',
+  boxShadow: 'rgb(41 18 18 / 34%) 0px 0px 29px 0px',
   colors: {
-    background: '#c18e7f',
-    content: '#d9b8ad',
-    navBar: '#e9d3cb',
-    sideMenu: '#e9d3cb',
+    background: 'linear-gradient(#c4a88f, #ca9487)',
+    content: '#dabda3',
+    navBar: '#e8dabf',
+    sideMenu: '#e8dabf',
     text: '#4a2b24',
   }
 };
@@ -14,21 +14,21 @@ export const autumnTheme: DefaultTheme = {
 export const springTheme: DefaultTheme = {
   boxShadow: 'rgb(0 0 0 / 35%) 0px 0px 29px 0px',
   colors: {
-    background: '#c5c98a',
+    background: 'linear-gradient(#b19f86,#be9882)',
     content: '#d9d7ad',
     navBar: '#f7ecc9',
     sideMenu: '#f7ecc9',
     text: '#2f341d',
   }
 };
-  
+
 export const winterTheme: DefaultTheme = {
-  boxShadow: 'rgb(149 157 165) 0px 0px 24px',
+  boxShadow: 'rgb(0 0 0 / 21%) 0px 0px 24px',
   colors: {
-    background: '#a4afc7',
-    content: '#c9cfd9',
-    navBar: '#d8dee1',
-    sideMenu: '#d8dee1',
+    background: 'linear-gradient(#b0b9d0, #8297be)',
+    content: '#b7c9da',
+    navBar: '#d5e2ee',
+    sideMenu: '#d5e2ee',
     text: '#3c4162',
   }
 };
