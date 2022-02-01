c8000bd0f01d4b0d91884f86ba26509cea3b69b1
Anna Kleszcz <anna.kleszcz123@gmail.com>
2022-02-01 19:29:00 +0100

    Make NavBar responsive

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,22 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 import { render, screen, within } from './utils/tests';
 
-describe('App component', () => {
-  test(`navigates to the 'About' section when 'About' is clicked in the navigation bar`, () => {
-    render(<App />);
-    userEvent.click(screen.getByRole('link', { name: 'About' }));
-    const main = screen.getByRole('main');
+jest.mock('react-responsive', () => {
+  return {
+    useMediaQuery: jest.fn(() => true),
+  };
+});
 
-    expect(within(main).getByText('About')).toBeInTheDocument();
+describe('App component', () => {
+  describe('on big screens', () => {
+    test(`navigates to the 'About' section when 'About' is clicked in the navigation bar`, () => {
+      render(<App />);
+
+      userEvent.click(screen.getByRole('link', { name: 'About', hidden: true }));
+      const main = screen.getByRole('main');
+
+      expect(within(main).getByText('About')).toBeInTheDocument();
+    });
   });
 
   test(`displays "Page not found" info for unknown routes`, async () => {
--- src/components/NavBar/index.test.tsx
+++ src/components/NavBar/index.test.tsx
@@ -1,28 +1,48 @@
 import userEvent from "@testing-library/user-event";
+import { useMediaQuery } from "react-responsive";
 import { render, screen, wrapInTheme } from "../../utils/tests";
 import NavBar from './index';
 
+jest.mock('react-responsive', () => {
+  return {
+    useMediaQuery: jest.fn(() => true),
+  };
+});
+
 describe('NavBar component', () => {
-  test('displays currently active NavLink with an underline and inactive ones without it', async () => {
-    render(wrapInTheme(<NavBar/>));
-    const HomeLink = screen.getByRole('link', { name: 'Home' });
-    const PortfolioLink = screen.getByRole('link', { name: 'Portfolio' });
-    const AboutLink = screen.getByRole('link', { name: 'About' });
-    const ContactLink = screen.getByRole('link', { name: 'Contact' });
-    const underlineStyle = 'text-decoration: underline;';
-    const noUnderlineStyle = 'text-decoration: none;';
+  describe('on big screens', () => {
+    test('displays currently active NavLink with an underline and inactive ones without it', async () => {
+      render(wrapInTheme(<NavBar/>));
+      const HomeLink = screen.getByRole('link', { name: 'Home' });
+      const PortfolioLink = screen.getByRole('link', { name: 'Portfolio' });
+      const AboutLink = screen.getByRole('link', { name: 'About' });
+      const ContactLink = screen.getByRole('link', { name: 'Contact' });
+      const underlineStyle = 'text-decoration: underline;';
+      const noUnderlineStyle = 'text-decoration: none;';
 
-    expect(HomeLink).toHaveStyle(underlineStyle);
-    expect(PortfolioLink).toHaveStyle(noUnderlineStyle);
-    expect(AboutLink).toHaveStyle(noUnderlineStyle);
-    expect(ContactLink).toHaveStyle(noUnderlineStyle);
+      expect(HomeLink).toHaveStyle(underlineStyle);
+      expect(PortfolioLink).toHaveStyle(noUnderlineStyle);
+      expect(AboutLink).toHaveStyle(noUnderlineStyle);
+      expect(ContactLink).toHaveStyle(noUnderlineStyle);
 
-    userEvent.click(AboutLink);
+      userEvent.click(AboutLink);
 
-    expect(HomeLink).toHaveStyle(noUnderlineStyle);
-    expect(PortfolioLink).toHaveStyle(noUnderlineStyle);
-    expect(AboutLink).toHaveStyle(underlineStyle);
-    expect(ContactLink).toHaveStyle(noUnderlineStyle);
+      expect(HomeLink).toHaveStyle(noUnderlineStyle);
+      expect(PortfolioLink).toHaveStyle(noUnderlineStyle);
+      expect(AboutLink).toHaveStyle(underlineStyle);
+      expect(ContactLink).toHaveStyle(noUnderlineStyle);
+    });
+  });
+
+
+  describe('on small screens', () => {
+    test('displays menu button', async () => {
+      (useMediaQuery as jest.Mock<boolean, []>).mockImplementation(() => false);
+      render(<NavBar />);
+
+      expect(await screen.findByLabelText('Navigation bar toggle')).toBeInTheDocument();
+
+    });
   });
 
 });
