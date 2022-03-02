22da0f9af260f48741e5631641c47c4d3d958d89
Anna Kleszcz <anna.kleszcz123@gmail.com>
2022-03-02 15:53:15 +0100

    Add tests for single image page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import userEvent from '@testing-library/user-event';
 import App from './App';
-import { render, screen, within } from './utils/tests';
+import { act, fireEvent, render, screen, within } from './utils/tests';
 
 jest.mock('react-responsive', () => {
   return {
@@ -15,16 +15,31 @@ describe('App component', () => {
       const primaryNav = screen.getByLabelText('Primary navigation');
 
       userEvent.click(within(primaryNav).getByRole('link', { name: 'About', hidden: true }));
-      const main = screen.getByRole('main');
 
-      expect(within(main).getByText('About')).toBeInTheDocument();
+      expect(within(screen.getByRole('main')).getByText('About')).toBeInTheDocument();
     });
   });
 
-  test(`displays "Page not found" info for unknown routes`, async () => {
+  test(`displays "Page not found" for unknown routes`, async () => {
     window.history.pushState({}, '', '/unknown-url');
     render(<App />);
 
     expect(await screen.findByText('Page not found')).toBeInTheDocument();
+  });
+
+  test(`displays a page with a single image`, async () => {
+    window.history.pushState({}, '', '/portfolio/1');
+    render(<App />);
+
+    expect(await within(screen.getByRole('main')).findAllByRole('img')).toHaveLength(1);
+  });
+
+  test(`displays "Failed to load image" for unknown image urls`, async () => {
+    window.history.pushState({}, '', '/portfolio/-2');
+    render(<App />);
+
+    fireEvent(await within(screen.getByRole('main')).findByRole('img'), new Event('error'));
+
+    expect(await screen.findByText('Failed to load image')).toBeInTheDocument();
   });
 });
